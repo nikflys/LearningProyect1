8d307608e5ccea1fa99ed5c64dd82105f8e574e7
nikflys <nikolasmt95@gmail.com>
2026-03-06 00:18:34 -0300

    Avances con respecto a las 5 opciones principales y tambien se implemento todo el tema de el manejo de los archivos .json dentro de main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import json
 import sys
 
-print("""1. Agregar datos del juego
+print("""1. Agregar un juego
 2. Ver todos los juegos
 3. Buscar en la lista de juegos
 4. Borrar juego de la lista
@@ -33,14 +33,60 @@ while True:
 
 
 if eleccion_usuario == 1:
-    pass
+    nombre_append = input("Cual es el nombre del juego? ")
+    release_append = input("Cuando salio el juego? Responde en formato YYYY-MM-DD ")
+    while True:
+        try:
+            precio_append = int(input("Cual es el preico del juego? Solo el numero entero, sin el $: "))
+            if type(precio_append) == int:
+                break
+        except ValueError:
+            print("Dato invalido, tenes que ingresar un numero dentro de las opciones disponibles")
+        except Exception as e:
+            print(f"Error inesperado: {e}")
+    nuevo_juego = {
+        "nombre" : nombre_append,
+        "release-date" : release_append,
+        "price" : precio_append
+    }
+    if datos is not None:
+        datos.append(nuevo_juego)
+        if archivo is not None:
+            archivo.close()
+            with open("datos_juegos.json", "w", encoding="utf-8") as archivo:
+                archivo = json.dump(datos, archivo, indent=4)
 elif eleccion_usuario == 2:
     print("Aca esta la lista con todos los juegos:")
     print(datos)
 elif eleccion_usuario == 3:
-    juego_a_buscar = input("Escribi el nombre del juego que queres buscar: ")
-    print(juego_a_buscar)
+    juego_a_buscar = input("Escribi el nombre del juego que queres buscar: ").lower()
+    if datos is not None:
+        encontrado = False
+        for juego in datos:
+            if juego_a_buscar == juego['nombre'].lower():
+                print(f"Encontrado! aca van los datos del juego {juego}")
+                encontrado = True
+                break
+        if encontrado == False:
+            print("Fijate de haberlo escrito bien, en caso de ser asi, lamentablemente ese juego no lo tenemos todavia.")
 elif eleccion_usuario == 4:
-    pass
+    juego_a_borrar = input("Escribi el nombre del juego que queres borrar: ").lower()
+    if datos is not None:
+        encontrado = False
+        i = 0
+        for juego in datos:
+            if juego_a_borrar == juego['nombre'].lower():
+                if datos is not None:
+                    datos.pop(i)
+                    if archivo is not None:
+                        archivo.close()
+                        with open("datos_juegos.json", "w", encoding="utf-8") as archivo:
+                            archivo = json.dump(datos, archivo, indent=4)
+                print(f"{juego['nombre']} fue borrado exitosamente!")
+                encontrado = True
+                break
+            i += 1
+        if encontrado == False:
+            print("Fijate de haberlo escrito bien, en caso de ser asi, ese juego no lo tenemos todavia y por ende no lo podemos borrar.")
 elif eleccion_usuario == 5:
     sys.exit()
